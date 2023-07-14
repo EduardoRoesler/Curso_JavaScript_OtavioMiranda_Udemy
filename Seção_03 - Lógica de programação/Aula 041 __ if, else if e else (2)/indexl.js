d3e79be4b29a193c1dae469fa86d67bb12f7facc
EduardoRoesler <eduardo.roesler@gmail.com>
2023-07-13 22:26:48 -0300

    Aula 041

--- Seção_03 - Lógica de programação/Aula 041 __ if, else if e else (2)/indexl.js
+++ Seção_03 - Lógica de programação/Aula 041 __ if, else if e else (2)/indexl.js
@@ -1,1 +1,13 @@
-const num = 10
+//O código não é analisado após o JS encontrar um parâmetro true, mesmo que outro parâmero abaixo também seja true
+const num = 20
+
+if (num>=0 && num<=10){
+    console.log('Num está entre 0 e 10')
+} else if (true){
+    console.log('código inerrompido')
+} else if (num>10 && num<=20) {
+    console.log('Num está entre 10 e 20')
+} else {
+    console.log('Num fora dos parâmetros')
+}
+
