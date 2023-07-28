ea83886d6a96dfe788b6a2ff52f31946382b6834
EduardoRoesler <eduardo.roesler@gmail.com>
2023-07-28 11:59:53 -0300

    Aula 060

---
+++ Seção_03 - Lógica de programação/Aula 060 __ Exercício com lógica de programação - 01/index.js
@@ -0,0 +1,10 @@
+//Escreva uma função que recebe dois números e retorna o maior deles
+//MINHA SOLUÇÃO
+const maior = (num1, num2) => Math.max(num1, num2)
+
+console.log(maior(2,7))
+
+//SOLUÇÃO DO PROFESSOR
+const max = (x, y) => x > y ? x : y
+
+console.log(max(4,9))
