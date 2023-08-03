6c2029fd15e1beadafabc13f25f11a0c86aa6f3a
EduardoRoesler <eduardo.roesler@gmail.com>
2023-08-02 23:49:44 -0300

    Aula 067

--- Seção_03 - Lógica de programação/Aula 067 __ Criando uma Lista de tarefas/assets/js/index.js
+++ Seção_03 - Lógica de programação/Aula 067 __ Criando uma Lista de tarefas/assets/js/index.js
@@ -5,14 +5,25 @@ taskInput.addEventListener('keypress', function(e){ //função para enviar ao pr
     if (e.keyCode === 13){addTask()} //13 é o keycode referente ao enter
 })
 
+taskContainer.addEventListener('click', function(e){
+    const taskRemoveBtn = e.target
+
+    if (taskRemoveBtn.classList.contains('removeBtn')) {
+        taskRemove(taskRemoveBtn)
+    }
+})
+
 const taskList = []
 
-function addTask(){
-    let task = taskInput.value //Cria task com o valor do input
+//ADICIONAR TAREFA
+function addTask(taskSave = taskInput.value){
+    console.log(taskSave)
+    let task = taskSave //Cria task com o valor do input
 
     if (!task) return //Verifica se existe conteudo no input
 
     taskList.push(task) //Adiciona task à taskList
+    saveTaskList()
 
     uiUpdate(task) //Envia task para uiUpdate
 
@@ -20,25 +31,72 @@ function addTask(){
 
 }
 
+//LIMPAR INPUT
 function taskInputCleaner (){
     taskInput.value = '' //Limpa input
     taskInput.focus() //Focus é uma função nativa do JS que coloca o cursor novamente no input
 }
 
+//ATUALIZAR UI
 function uiUpdate(task){
 
-    const taskRow = document.createElement('li') //Cria li
-    taskRow.classList.add('taskRow') //Adiciona à li - class = "taskRow"
+    const taskRow = createTaskRow()
+    const taskRemoveBtn = createTaskRemoveBtn()
+    const taskContent = createTaskContent(task)
 
-    const taskRemove = document.createElement('button') //Cria botão apagar
-    taskRemove.classList.add('removeBtn') //Adiciona ao botão apagar - class = "remobeBtn"
-    taskRemove.innerText = 'Apagar' //Adiciona o texto do botão apagar
-
-    const taskItem = document.createElement('span') //Cria span
-    taskItem.textContent = task //Adiciona task como texto para o span
-
-    taskRow.appendChild(taskRemove) //Adiciona botão ao li
-    taskRow.appendChild(taskItem) //Adiciona span ao li
+    taskRow.appendChild(taskRemoveBtn) //Adiciona botão ao li
+    taskRow.appendChild(taskContent) //Adiciona span ao li
     taskContainer.appendChild(taskRow) //Adiciona li ao ul
-    
 }
+
+//CRIAR TASK ROW
+function createTaskRow(){
+    const taskRow = document.createElement('li') //Cria li
+    taskRow.classList.add('taskRow') //Adiciona à li - class = "taskRow" 
+
+    return taskRow
+}
+
+//CRIAR TASK REMOVE BTN
+function createTaskRemoveBtn(){
+    const taskRemoveBtn = document.createElement('button') //Cria botão apagar
+    taskRemoveBtn.innerText = 'Apagar' //Adiciona o texto do botão apagar
+    taskRemoveBtn.setAttribute('class', 'removeBtn') //Adiciona ao botão apagar - class = "remobeBtn" //Com SetAtribute, posso adicionar qualquer atributo, seja classe, id, name...
+
+    return taskRemoveBtn
+}
+
+//CRIAR TASK CONTENT
+function createTaskContent(task){
+    const taskContent = document.createElement('span') //Cria span
+    taskContent.textContent = task //Adiciona task como texto para o span
+
+    return taskContent
+}
+
+//REMOVE TAREFA
+function taskRemove(element){
+    const task = element.nextElementSibling.textContent //Proximo elemento irmão do botão
+    const taskIndex = taskList.indexOf(task) //Busca índice do array referente à task
+    taskList.splice(taskIndex, 1) //Remove a task do array
+    saveTaskList()
+    console.log(taskList)
+
+    element.parentElement.remove() //Remove a Task Row
+}
+
+function saveTaskList(){
+    const taskListJSON = JSON.stringify(taskList) //Converte array para JSON
+    localStorage.setItem('taskList', taskListJSON) //Salva no localStorage o JSON (Só posso salvar JSON no localStorage)
+}
+
+function handleSaveTaskList(){
+    const saveTaskList = localStorage.getItem('taskList')
+    const taskList = JSON.parse(saveTaskList)
+
+    for (taskSave of taskList){
+        addTask(taskSave)
+    }
+}
+
+handleSaveTaskList()
