734333f1ea8e1458c1806adcaad0007d77d3b45e
EduardoRoesler <eduardo.roesler@gmail.com>
2023-08-08 00:07:10 -0300

    Aula 071

---
+++ Seção_04 - JavaScript Funções (Avançado)/Aula 071 __ Escopo léxico/index.js
@@ -0,0 +1,7 @@
+/*
+- Escopo léxico refere-se à região do código, na qual uma determinada variável é visível e pode ser acessada. 
+- O escopo determina onde uma variável pode ser usada e reconhecida pelo compilador. 
+- O escopo define a área onde uma variável é válida e pode armazenar ou recuperar informações. 
+- Variáveis definidas em um escopo específico não são visíveis ou acessíveis em outros escopos, a menos que haja um mecanismo explícito de compartilhamento ou passagem de variáveis entre esses escopos. 
+- Isso ajuda a evitar conflitos e ambiguidades, garantindo que diferentes partes de um programa possam usar nomes de variáveis sem interferir umas nas outras.
+*/
