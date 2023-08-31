e5842dd815fa54d608659c768268f003c555933b
EduardoRoesler <eduardo.roesler@gmail.com>
2023-08-30 22:30:28 -0300

    Aula 094

--- Seção_06 - JavaScript Objetos e Prototypes (Avançado)/Aula 092 __ Métodos úteis para objetos/index.js
+++ Seção_06 - JavaScript Objetos e Prototypes (Avançado)/Aula 092 __ Métodos úteis para objetos/index.js
@@ -1,3 +1,65 @@
-/*
+//COPIAR VALORES//
+//Spread Operator
+const produto01 = {nome: 'Caneca', preco: 1.8}
+const produto02 = {...produto01}
 
-*/
+produto01.nome = 'Toalha'
+
+console.log("Produto01:", produto01)
+console.log("Outro Produto:", produto02)
+
+//Object.assign()
+const produto10 = {genero: 'Homem', idade: 20}
+const produto11 = Object.assign({}, produto10, {concatenar: 'Fim do objeto'}) // O primeiro argumento é o alvo (um objeto vazio para copiar), o segundo é a fonte, o terceiro adiciona propriedades ao objeto resultante
+// No primeiro argumento, você pode fornecer um objeto alvo para onde as propriedades são copiadas; neste exemplo, um objeto vazio é usado para criar um novo objeto com as propriedades da fonte
+// As propriedades da fonte (segundo argumento) são copiadas para o objeto alvo; isso significa que o objeto alvo receberá cópias das propriedades da fonte
+// No terceiro argumento, você pode adicionar mais propriedades ao objeto resultante; neste caso, está adicionando a propriedade 'concatenar' com o valor 'Fim do objeto'
+// As propriedades adicionadas ao objeto resultante através do terceiro argumento são colocadas no final do objeto, após as propriedades já copiadas
+// Se dois ou mais objetos tiverem chaves de mesmo nome, o valor final da chave será o do último objeto adicionado
+
+
+produto10.genero = 'Mulher'
+
+console.log("Produto10:", produto10)
+console.log("Outro Produto11:", produto11)
+
+//Cópia manual
+const produto20 = {nome: 'Computador', preco: 4000} 
+const produto21 = {nome: produto20.nome, preco: produto20.preco} //É a menos indicada, porém pode ser útil quando precisarmos copiar apenas algumas chaves
+
+produto20.nome = 'Mouse'
+
+console.log("Produto01:", produto20)
+console.log("Outro Produto:", produto21)
+
+//CONGELAR OBJETO
+Object.freeze(produto11) //Agora é ipossível aterar os valores do objeto, bem como deletar ou inserir chaves
+
+//RETORNAR PROPRIEDADES DO OBJETO
+console.log('PROPRIEDADES DE TODO O OBJETO:', Object.getOwnPropertyDescriptors(produto01))
+
+Object.defineProperty(produto01, 'nome', { //Alterei as propriedades dessa chave, abaixo exibirei apenas elas
+    value: 'Valor alterado',
+    writable: false,
+    enumerable: false,
+    configurable: false
+})
+
+console.log('PROPRIEDADES DA CHAVE "genero":', Object.getOwnPropertyDescriptor(produto01, 'nome'))
+
+//EXIBIR AS CHAVES DO OBJETO
+console.log('CHAVES:', Object.keys(produto20))
+
+//EXIBIR OS VALORES DO OBJETO
+console.log('VALORES:', Object.values(produto20))
+
+//EXIBIR TODAS AS ENTRADAS DO OBJETO
+console.log('ENTRADAS:', Object.entries(produto20)) //Retorna um array, com arrays para cada chave. Isso permite iterações
+
+for(let entradas of Object.entries(produto20)){ //Iteração entre as entradas do objeto
+    console.log(entradas)
+}
+
+for(let [chave, valor] of Object.entries(produto20)){ //Posso fazer destructuring direto no for
+    console.log(valor)
+}
--- Seção_06 - JavaScript Objetos e Prototypes (Avançado)/Aula 093 __ Prototypes/index.js
+++ Seção_06 - JavaScript Objetos e Prototypes (Avançado)/Aula 093 __ Prototypes/index.js
@@ -1,3 +1,62 @@
 /*
+PROTOTYPES
 
+1. Definição:
+   - Protótipos são um mecanismo fundamental em JavaScript que permite a herança de propriedades e métodos entre objetos.
+
+2. Herança Prototipal:
+   - Em vez de usar classes como em linguagens orientadas a objetos tradicionais, JavaScript usa protótipos para compartilhar comportamentos entre objetos.
+
+3. Objeto Prototype:
+   - Todo objeto em JavaScript tem uma propriedade chamada `prototype`.
+   - Ela é uma referência ao objeto a partir do qual o objeto atual herda propriedades e métodos.
+
+4. Herança por Cadeia de Protótipos:
+   - Quando uma propriedade ou método não é encontrado em um objeto, JavaScript procura na cadeia de protótipos até encontrar ou chegar ao topo.
+
+5. Herança e Construtores:
+   - Funções construtoras e classes podem ser usadas para criar objetos com protótipos.
+   - Quando um objeto é criado a partir de uma função construtora, ele herda propriedades do protótipo dessa função.
+
+6. Prototype Chain:
+   - É a sequência de objetos protótipos usada para buscar propriedades e métodos.
+   - O último elo na cadeia é o objeto global (geralmente `Object.prototype`).
+
+7. Método `Object.create()`:
+   - Permite criar um novo objeto com um protótipo especificado.
+   - Usado para estabelecer protótipos de forma mais direta.
+
+8. Vantagens:
+   - Reutilização de Código: Permite compartilhar comportamentos entre objetos sem duplicação.
+   - Flexibilidade: Permite adicionar ou substituir propriedades e métodos em protótipos.
+
+9. Desvantagens:
+   - Pode ser mais complexo de entender em comparação com a herança baseada em classes.
+
+10. Melhores Práticas:
+    - Use protótipos quando desejar compartilhar propriedades e métodos entre objetos.
+    - Entenda a cadeia de protótipos ao acessar propriedades e métodos.
+
+O uso de protótipos é uma parte fundamental da linguagem JavaScript. Entender como a herança prototípica funciona é crucial para criar objetos que compartilham comportamentos e propriedades de maneira eficiente e flexível.
 */
+
+// FUNÇÃO CONSTRUTORA (Pode ser imaginada como um molde para as instâncias)
+function Pessoa(nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    // this.nomeCompleto = ()=>`${this.nome} ${this.sobrenome}` - Criando um método desta maneira, cada instância de objeto criada a partir desta função terá sua própria cópia do método, o que pode ser ineficiente. Uma solução melhor é adicionar o método ao prototype, tornando-o acessível a todas as instâncias.
+}
+
+// Adicionando o método nomeCompleto ao protótipo da função construtora Pessoa
+Pessoa.prototype.nomeCompleto = function() {
+    return `${this.nome} ${this.sobrenome}`;
+}
+
+// INSTÂNCIA
+const pessoa01 = new Pessoa('Eduardo', 'Roesler');
+
+console.log(pessoa01);
+console.log(pessoa01.nomeCompleto); // Mostra a definição da função nomeCompleto
+console.log(pessoa01.nomeCompleto()); // Invoca o método nomeCompleto para obter o nome completo
+// É importante observar a cadeia de pesquisa (prototype chain) do JavaScript. Neste caso, será: pessoa01 -> Pessoa.prototype -> Object.prototype
+
