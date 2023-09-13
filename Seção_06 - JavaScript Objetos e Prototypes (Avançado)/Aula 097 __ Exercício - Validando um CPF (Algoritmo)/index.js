fbbc8b75e771cca8dca6e83523be4848f99d9d10
EduardoRoesler <eduardo.roesler@gmail.com>
2023-09-12 23:14:02 -0300

    Aula 097

--- Seção_06 - JavaScript Objetos e Prototypes (Avançado)/Aula 097 __ Exercício - Validando um CPF (Algoritmo)/index.js
+++ Seção_06 - JavaScript Objetos e Prototypes (Avançado)/Aula 097 __ Exercício - Validando um CPF (Algoritmo)/index.js
@@ -1,11 +1,54 @@
 function validarCPF(cpfString){
-    cpfLimpo = cpfString.replace(/\D+/g, '')
-    console.log(cpfLimpo)
 
+    const cpfArray = limparCPF(cpfString)
+    
+    const base = cpfArray.slice(0,9)
+    
+    let primeiroVerificador = digitoVerificador(base)
+    base.push(primeiroVerificador)
+
+    let segundoVerificador = digitoVerificador(base)
+    base.push(segundoVerificador)
+
+    const isValid = JSON.stringify(base) === JSON.stringify(cpfArray)
+
+    isValid ? console.log('CPF VÁLIDO') : console.log('CPF INVÁLIDO')
+}
+
+function limparCPF(cpf){
+    cpfLimpo = cpf.replace(/\D+/g, '')
     cpfArray = Array.from(cpfLimpo)
-    console.log(cpfArray)
+    return cpfArray
+}
 
 
+function digitoVerificador(cpf){
+
+    let operador = cpf.length + 1
+
+    let cpfOperacao = cpf
+    .map(valor => {
+        valor = valor * operador
+        operador--
+        return valor
+    })
+    .reduce((acumulador, valor)=>{
+        acumulador = valor + acumulador
+        resto = acumulador%11
+        return resto
+    })
+
+    console.log(resto)
+
+    let verificador
+    if(cpfOperacao<2){
+        verificador = 0
+    } else {
+        verificador = 11 - cpfOperacao
+    }
+
+    console.log(verificador)
+    return String(verificador)
 }
 
 validarCPF('036.432.240-36')
